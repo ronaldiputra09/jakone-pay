b2932a513ff6fe16626e069f99301d9a2a89668c
ronaldiputra09 <emailnyaronal@gmail.com>
2024-10-16 20:04:01 +0700

    add : splash screen

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 class SplashController extends GetxController {
   @override
   void onInit() {
+    navigateToHome();
     super.onInit();
   }
 
@@ -14,5 +15,15 @@ class SplashController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  // func to navigate to another page after splash screen 3 seconds
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.offNamed('/home');
+      },
+    );
   }
 }
--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:jakone_pay/app/data/assets.dart';
+import 'package:jakone_pay/app/data/colors.dart';
+import 'package:jakone_pay/app/data/fonts.dart';
 
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
-  const SplashView({super.key});
+  SplashView({super.key});
+  final splashC = Get.put(SplashController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(bgSplash),
+          fit: BoxFit.cover,
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Image.asset(
+                  logo,
+                  width: 286,
+                ),
+              ),
+            ),
+            Image.asset(
+              ojkLps,
+              width: 100,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'powered by BANK DKI\n2023',
+              style: secondaryFont.copyWith(
+                fontSize: 11,
+                color: white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
