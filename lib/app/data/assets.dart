cd93649698de9df1b707d8bc965880b0251fa059
ronaldiputra09 <emailnyaronal@gmail.com>
2024-10-16 19:37:16 +0700

    add : path assets

---
+++ lib/app/data/assets.dart
@@ -0,0 +1,33 @@
+/// IMAGES PATH
+const String ancol = 'assets/images/ancol.png';
+const String bgHeader = 'assets/images/bg_header.png';
+const String bgSplash = 'assets/images/bg_splash.png';
+const String event1 = 'assets/images/event1.png';
+const String event2 = 'assets/images/event2.png';
+const String inijakarta = 'assets/images/inijakarta.png';
+const String logo = 'assets/images/logo.png';
+const String monas = 'assets/images/monas.png';
+const String monumen = 'assets/images/monumen.png';
+const String ojkLps = 'assets/images/ojk_lps.png';
+const String primata = 'assets/images/primata.png';
+const String qris = 'assets/images/qris.png';
+
+/// ICONS PATH
+const String cards = 'assets/icons/cards.png';
+const String didYouKnow = 'assets/icons/did_you_know.png';
+const String en = 'assets/icons/en.png';
+const String event = 'assets/icons/event.png';
+const String explore = 'assets/icons/explore.png';
+const String help = 'assets/icons/help.png';
+const String icCard = 'assets/icons/ic_card.png';
+const String icEvent = 'assets/icons/ic_event.png';
+const String icHome = 'assets/icons/ic_home.png';
+const String icNote = 'assets/icons/ic_note.png';
+const String icNotification = 'assets/icons/ic_notification.png';
+const String icQris = 'assets/icons/ic_qris.png';
+const String icUser = 'assets/icons/ic_user.png';
+const String id = 'assets/icons/id.png';
+const String jakcard = 'assets/icons/jakcard.png';
+const String lestgo = 'assets/icons/lestgo.png';
+const String profile = 'assets/icons/profile.png';
+const String topup = 'assets/icons/topup.png';
