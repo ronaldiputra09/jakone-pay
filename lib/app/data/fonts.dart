df19571ce87392b222770bb1c07cb14ac9b59848
ronaldiputra09 <emailnyaronal@gmail.com>
2024-10-16 19:42:16 +0700

    add : - add package google_fonts - fonts constant

---
+++ lib/app/data/fonts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle primaryFont = GoogleFonts.nunito();
+TextStyle secondaryFont = GoogleFonts.inter();
+
+FontWeight black = FontWeight.w800;
+FontWeight bold = FontWeight.w700;
+FontWeight semiBold = FontWeight.w600;
+FontWeight medium = FontWeight.w500;
+FontWeight regular = FontWeight.w400;
+FontWeight light = FontWeight.w300;
